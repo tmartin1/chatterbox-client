78daa4af4cab3c12b8a612ed106e5b8d19e31620
tmartin1 <timothy.m.martin@gmail.com>
2014-12-29 17:34:28 -0800

    Some progress made on SpecRunner. Breaking for dinner.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,57 +1,115 @@
-// Set default url for $.ajax calls to https://api.parse.com/1/classes/chatterbox
-$.ajaxSetup({
-	url: 'https://api.parse.com/1/classes/chatterbox'
-});
+var app = {};
 
-// Fetches messages from the server.
-var getMessages = function() {
-	$.ajax({
-	  type: 'GET',
-	  data: 'data',
-	  contentType: 'application/json',
-	  success: function (data) {
-	    displayMessages(data);
-	  },
-	  error: function (data) {
-	    console.error('chatterbox: Failed to retrieve messages');
-	  }
-	});
+app.init = function(){
+  console.log("running");
+  var context = this;
+  setInterval(function() {
+    context.refreshMessages();
+  },1000);
 };
 
-// Displays messages retrieved from server.
-var displayMessages = function(data) {
-	console.log(data['results']);
-	// for (var obj in data) {
-	// 	console.log(data[obj]);
-	// }
-};
-
-// Refreshes messages based on time interval.
-var refreshRate = 3000;
-setInterval(function() {
-	getMessages();
-},refreshRate);
-
-
-// Allows users to post messages to the server.
-// Messages must be in the following format:
-//		var message = {
-//		  'username': 'shawndrost',
-//		  'text': 'trololo',
-//		  'roomname': '4chan'
-//		};
-/*
-$.ajax({
-  // always use this url
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
+$('#newChat').keypress(function(key){
+  if(key.witch === 13){
+    var message = {
+      'username': "",
+      'text': this.val(),
+      'roomname': ""
+    };
+    this.send(message);
   }
 });
-*/
+
+app.send = function(message){
+  $.ajax({
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function(){
+  console.log('running fetch');
+  $.ajax({
+    type: 'GET',
+    data: 'data',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Messages recieved');
+      // this.loadMessages(data['results']);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to recieve messages');
+    }
+  });
+};
+
+app.clearMessages = function(){
+  $('#chats').empty();
+};
+
+app.addMessage = function(message){
+  // send message to server
+  // refresh message display
+  this.send(message);
+  this.fetch();
+};
+
+// populates $('#chats') with most recent messages
+app.loadMessages = function(results) {
+  // call app.fetch and store as array
+  // iterate through array from above
+    // display username with class 'username'
+      // if username is on friendList, display in bold
+      // else onclick=addFriend(this)
+    // display chat
+  console.log(results);
+  for (var i=0; i<results.length; i++) {
+    var username = results[i].username;
+    var text = results[i].text;
+    var room = results[i].roomname;
+
+    var newMsg = $('<li/>');
+    var user = $('<div/>')
+      .val(username)
+      .addClass('username');
+    user.attr('onclick',addFriend(username));
+
+    var msgBody = $('<div/>').addClass('msgBody');
+
+    user.appendTo(newMsg);
+    msgBody.appendTo(newMsg);
+    newMsg.appendTo('#chats');
+  }
+};
+
+app.refreshMessages = function() {
+  this.clearMessages();
+  this.fetch();
+  console.log('refreshing');
+};
+
+app.addRoom = function(room){
+  var newRoom = $('<li/>').appendTo('#roomSelect');
+};
+
+app.addFriend = function(user){
+  var newFriend = $('<li/>')
+    .val(user)
+    .appendTo('#friendList');
+};
+
+// $('.username').on('click', funciton(){
+//   app.addFriend(this.val());
+// });
+
+app.handleSubmit = function(){
+
+};
